48f44b80ebeac8d49dec26c2ea6680be104a3c05
Asaf Kfir <asafkfir11@gmail.com>
2022-03-01 18:46:41 +0200

    Add printPolyMul, adapt main, printing tests

--- main.c
+++ main.c
@@ -10,20 +10,35 @@ typedef struct monom {
   int power;
 } Monom;
 
-Monom* scanPolynomialMonoms(int* monomsArraySize);
+Monom* getPolynom(int* monomsArraySize);
 Monom* sumPolynomialMonomsWithSamePower(Monom polynomialMonomArray[], int* size);
+void printPolySum(Monom* polynom1, int monomsArraySize1, Monom* polynom2, int monomsArraySize2);
+void printPolyMul(Monom* polynom1, int monomsArraySize1, Monom* polynom2, int monomsArraySize2);
 void copyArr(Monom src[], Monom dest[], int size);
 void mergeDescending(Monom arr1[], int size1, Monom arr2[], int size2, Monom tmpArr[]);
 void mergeSort(Monom arr[], int size);
 void printPolynomial(Monom polynomialMonomArray[], int monomsArraySize);
 
-int main() {
-  int monomsArraySize;
-  Monom* polynomialMonomArray = scanPolynomialMonoms(&monomsArraySize);
-  mergeSort(polynomialMonomArray, monomsArraySize);
-  polynomialMonomArray = sumPolynomialMonomsWithSamePower(polynomialMonomArray, &monomsArraySize);
-  printPolynomial(polynomialMonomArray, monomsArraySize);
-  free(polynomialMonomArray);
+void main() {
+  Monom *polynom1, *polynom2;              // The input polynoms
+  unsigned int polynom1Size, polynom2Size; // The size of each polynom
+
+  printf("Please enter the first polynom:\n");
+  polynom1 = getPolynom(&polynom1Size); // get polynom 1
+
+  printf("Please enter the second polynom:\n");
+  polynom2 = getPolynom(&polynom2Size); // get polynom 2
+
+  printf("The multiplication of the polynoms is:\n"); // print the multiplication
+  printPolyMul(polynom1, polynom1Size, polynom2, polynom2Size);
+  printf("\n");
+
+  printf("The sum of the polynoms is:\n"); // print the sum
+  printPolySum(polynom1, polynom1Size, polynom2, polynom2Size);
+  printf("\n");
+
+  free(polynom1); // releasing all memory allocations
+  free(polynom2);
 }
 
 void copyArr(Monom src[], Monom dest[], int size) {
@@ -88,10 +103,9 @@ void mergeSort(Monom arr[], int size) {
   return;
 }
 
-Monom* scanPolynomialMonoms(int* monomsArraySize) {
+Monom* getPolynom(int* monomsArraySize) {
   int i, arrSize = 0;
   char lastInput;
-  // TODO: Fix this to apply for unknown length
   int polynomialIntegerArray[MAX_LENGTH];
   Monom* polynomialMonomArray;
 
@@ -159,7 +173,13 @@ void printPolynomial(Monom polynomialMonomArray[], int monomsArraySize) {
         }
       }
 
-      printf("%d", currentCoefficient);
+      if (currentCoefficient != 1) {
+        if (currentCoefficient == -1 && currentPower != 0) {
+          printf("-");
+        } else {
+          printf("%d", currentCoefficient);
+        }
+      }
 
       if (currentPower != 0) {
         printf("x");
@@ -171,6 +191,57 @@ void printPolynomial(Monom polynomialMonomArray[], int monomsArraySize) {
   }
 
   printf("\n");
+
+  return;
+}
+
+void printPolySum(Monom* polynom1, int monomsArraySize1, Monom* polynom2, int monomsArraySize2) {
+  Monom* polynom1Copy = (Monom*)malloc(monomsArraySize1 * sizeof(Monom));
+  Monom* polynom2Copy = (Monom*)malloc(monomsArraySize2 * sizeof(Monom));
+  Monom* polynomSum = (Monom*)malloc((monomsArraySize1 + monomsArraySize2) * sizeof(Monom));
+  int i, j, polynomSumSize;
+
+  copyArr(polynom1, polynom1Copy, monomsArraySize1);
+  copyArr(polynom2, polynom2Copy, monomsArraySize2);
+
+  mergeSort(polynom1Copy, monomsArraySize1);
+  mergeSort(polynom2Copy, monomsArraySize2);
+
+  polynomSumSize = monomsArraySize1 + monomsArraySize2;
+  mergeDescending(polynom1Copy, monomsArraySize1, polynom2Copy, monomsArraySize2, polynomSum);
+
+  polynomSum = sumPolynomialMonomsWithSamePower(polynomSum, &polynomSumSize);
+
+  printPolynomial(polynomSum, polynomSumSize);
+
+  free(polynom1Copy);
+  free(polynom2Copy);
+  free(polynomSum);
+
+  return;
+}
+
+void printPolyMul(Monom* polynom1, int monomsArraySize1, Monom* polynom2, int monomsArraySize2) {
+  int i, j, polynomMulSize, polynomMulWriteIndex = 0;
+  polynomMulSize = monomsArraySize1 * monomsArraySize2;
+
+  Monom* polynomMul = (Monom*)malloc(polynomMulSize * sizeof(Monom));
+
+  for (i = 0; i < monomsArraySize1; i++) {
+    for (j = 0; j < monomsArraySize2; j++) {
+      polynomMul[polynomMulWriteIndex].coefficient = polynom1[i].coefficient * polynom2[j].coefficient;
+      polynomMul[polynomMulWriteIndex].power = polynom1[i].power + polynom2[j].power;
+      polynomMulWriteIndex++;
+    }
+  }
+
+  mergeSort(polynomMul, polynomMulSize);
+
+  polynomMul = sumPolynomialMonomsWithSamePower(polynomMul, &polynomMulSize);
+
+  printPolynomial(polynomMul, polynomMulSize);
+
+  free(polynomMul);
 
   return;
 }
