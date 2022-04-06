2fa284a9db672e2d0a21226aa32e3f7d259d1f6e
Asaf Kfir <asafkfir11@gmail.com>
2022-04-06 19:39:28 +0300

    Separation to different files

--- trees.c
+++ trees.c
@@ -1,11 +1,11 @@
 #include "trees.h"
-#include "common.h"
 
-TreeNode* createTreeNode(int data, TreeNode* left, TreeNode* right) {
+TreeNode* createTreeNode(int data, TreeNode* parent, TreeNode* left, TreeNode* right) {
   TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
   checkAllocation(newNode);
 
   newNode->data = data;
+  newNode->parent = parent;
   newNode->left = left;
   newNode->right = right;
 
@@ -14,6 +14,7 @@ TreeNode* createTreeNode(int data, TreeNode* left, TreeNode* right) {
 
 void freeTree(Tree tr) {
   freeTreeRec(tr.root);
+  freeListRec(tr.leafList.head);
 }
 
 void freeTreeRec(TreeNode* root) {
@@ -26,67 +27,129 @@ void freeTreeRec(TreeNode* root) {
   }
 }
 
-bool buildExpressionTree(char* expression, Tree* tree) {
-  tree->root = buildExpressionTreeRec(expression);
-
-  return (tree->root != NULL);
+void printTreeInorder(Tree tr) {
+  printTreeInorderRec(tr.root);
 }
 
-TreeNode* buildExpressionTreeRec(char* expression) {
-  int operatorIndex;
-  int expressionLength = strlen(expression);
-  if (expressionLength == 0) {
-    return NULL;
-  } else if (expressionLength == 1) {
-    if (!(isValidInteger(expression[0]))) {
-      printf("The input expression is not valid\n");
-      exit(1);
-    } else {
-      int nodeData = expression[0] - '0';
+void printTreeInorderRec(TreeNode* root) {
+  if (root == NULL)
+    return;
 
-      return createTreeNode(nodeData, NULL, NULL);
+  printTreeInorderRec(root->left);
+  printf("%d ", root->data);
+  printTreeInorderRec(root->right);
+}
+
+void printLeafList(Tree tr) {
+  printLeafListRec(tr.leafList.head);
+}
+
+void updateSubNodesParents(TreeNode* node) {
+  if (node->left != NULL) {
+    node->left->parent = node;
+  }
+  if (node->right != NULL) {
+    node->right->parent = node;
+  }
+}
+
+Tree BuildTreeFromArrayWithLeafList(int* arr, int size) {
+  Tree tr;
+  initEmptyList(&tr.leafList);
+
+  tr.root = BuildTreeFromArrayWithLeafListRec(&tr, arr, size);
+
+  return tr;
+}
+
+TreeNode* BuildTreeFromArrayWithLeafListRec(Tree* tr, int* arr, int size) {
+  TreeNode* parentNode = NULL;
+  int newNodeData = arr[size / 2];
+
+  if (newNodeData == -1) {
+    return NULL;
+  }
+
+  if (size == 1) {
+    insertDataToEndOfList(&(tr->leafList), newNodeData);
+    parentNode = createTreeNode(newNodeData, NULL, NULL, NULL);
+    return parentNode;
+  }
+
+  TreeNode* leftNode = BuildTreeFromArrayWithLeafListRec(tr, arr, (size / 2));
+  TreeNode* rightNode = BuildTreeFromArrayWithLeafListRec(tr, arr + (size / 2) + 1, size / 2);
+
+  if (leftNode == NULL && rightNode == NULL) {
+    insertDataToEndOfList(&(tr->leafList), newNodeData);
+  }
+
+  parentNode = createTreeNode(newNodeData, NULL, leftNode, rightNode);
+  updateSubNodesParents(parentNode);
+
+  return parentNode;
+}
+
+Tree AddLeaf(Tree tr, TreeNode* p, int branchSelect, int data) {
+  TreeNode* newNode = createTreeNode(data, p, NULL, NULL);
+  if (branchSelect == LEFT) {
+    p->left = newNode;
+  } else if (branchSelect == RIGHT) {
+    p->right = newNode;
+  } else {
+    printf("Wrong branchSelect option. Exiting...");
+    exit(1);
+  }
+
+  bool isReplaced = replaceListNodeInLeafListRec(tr.leafList.head, p->data, data);
+
+  if (!isReplaced) {
+    if (branchSelect == LEFT) {
+      insertDataToStartOfList(&(tr.leafList), data);
+    } else if (branchSelect == RIGHT) {
+      insertDataToEndOfList(&(tr.leafList), data);
     }
   }
 
-  operatorIndex = findParentOperatorIndex(expression);
-  if (operatorIndex == -1) {
+  return tr;
+}
+
+TreeNode* findParent(Tree tr, int parentData, int branchSelect) {
+  return findParentRec(tr.root, parentData, branchSelect);
+}
+
+TreeNode* findParentRec(TreeNode* root, int parentData, int branchSelect) {
+  int nodeData;
+
+  if (root == NULL) {
     return NULL;
   }
 
-  char* leftSubExpression = createSubExpressionByIndices(expression, 1, operatorIndex - 1);
-  char* rightSubExpression = createSubExpressionByIndices(expression, operatorIndex + 1, strlen(expression) - 2);
+  nodeData = root->data;
 
-  TreeNode* leftNode = buildExpressionTreeRec(leftSubExpression);
-  TreeNode* rightNode = buildExpressionTreeRec(rightSubExpression);
-
-  TreeNode* root = createTreeNode(expression[operatorIndex], leftNode, rightNode);
-
-  return root;
-}
-
-double calcExpression(Tree tr) {
-  return calcExpressionRec(tr.root);
-}
-
-double calcExpressionRec(TreeNode* root) {
-  if (root == NULL)
-    return 0;
-
-  double left = calcExpressionRec(root->left);
-  double right = calcExpressionRec(root->right);
-
-  switch (root->data) {
-    case '+':
-      return left + right;
-    case '-':
-      return left - right;
-    case '*':
-      return left * right;
-    case '/':
-      return left / right;
-    case '%':
-      return (int)left % (int)right;
-    default:
-      return root->data;
+  if (nodeData != parentData) {
+    return findParentInSubnodes(root, parentData, branchSelect);
   }
+
+  switch (branchSelect) {
+    case LEFT:
+      if (root->left == NULL)
+        return root;
+    case RIGHT:
+      if (root->right == NULL)
+        return root;
+    default:
+      return findParentInSubnodes(root, parentData, branchSelect);
+  }
+}
+
+TreeNode* findParentInSubnodes(TreeNode* root, int parentData, int branchSelect) {
+  TreeNode *leftNode = NULL, *rightNode = NULL;
+
+  leftNode = findParentRec(root->left, parentData, branchSelect);
+  if (leftNode != NULL) {
+    return leftNode;
+  } else {
+    rightNode = findParentRec(root->right, parentData, branchSelect);
+    return rightNode;
+  };
 }
--- trees.h
+++ trees.h
@@ -1,23 +1,36 @@
 #pragma once
+#include "common.h"
+#include "lists.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct TreeNode {
-  unsigned int data;
-  struct TreeNode* right;
-  struct TreeNode* left;
+#define LEFT 0
+#define RIGHT 1
+
+typedef struct treeNode {
+  int data;
+  struct treeNode* parent;
+  struct treeNode* left;
+  struct treeNode* right;
 } TreeNode;
 
-typedef struct Tree {
+typedef struct tree {
   TreeNode* root;
+  List leafList; /*רשימה מקושרת של כל העלים בעץ*/
 } Tree;
 
-bool buildExpressionTree(char* expression, Tree* tree);
-TreeNode* buildExpressionTreeRec(char* expression);
-double calcExpression(Tree tr);
-double calcExpressionRec(TreeNode* root);
-TreeNode* createTreeNode(int data, TreeNode* left, TreeNode* right);
+Tree BuildTreeFromArrayWithLeafList(int* arr, int size);
+TreeNode* BuildTreeFromArrayWithLeafListRec(Tree* tr, int* arr, int size);
+void updateSubNodesParents(TreeNode* node);
+Tree AddLeaf(Tree tr, TreeNode* p, int branchSelect, int data);
+TreeNode* findParent(Tree tr, int parentData, int branchSelect);
+TreeNode* findParentRec(TreeNode* root, int parentData, int branchSelect);
+void printTreeInorder(Tree tr);
+void printTreeInorderRec(TreeNode* root);
+void printLeafList(Tree tr);
+TreeNode* findParentInSubnodes(TreeNode* root, int parentData, int branchSelect);
+TreeNode* createTreeNode(int data, TreeNode* parent, TreeNode* left, TreeNode* right);
 void freeTree(Tree tr);
 void freeTreeRec(TreeNode* root);
