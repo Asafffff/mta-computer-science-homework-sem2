b5ef195f207ead056adf07251960583a89452780
Asaf Kfir <asafkfir11@gmail.com>
2022-05-21 17:23:40 +0300

    Implement Exercise 3

--- common.c
+++ common.c
@@ -30,3 +30,93 @@ void* increaseArraySizeIfFull(void* arr, int* logSize, int* phySize, unsigned lo
 
   return arr;
 }
+
+void* scramble(void* arr, int ElemSize, int n, int* indArr) {
+  int i;
+  BYTE* scrambledArr = (BYTE*)malloc(ElemSize * n);
+  checkAllocation(scrambledArr);
+
+  for (i = 0; i < n; i++) {
+    int newIndex = indArr[i];
+    BYTE* scrambledAddress = scrambledArr + i * ElemSize;
+    BYTE* originalAddress = (BYTE*)arr + newIndex * ElemSize;
+    memcpy(&scrambledArr[i * ElemSize], &((BYTE*)arr)[newIndex * ElemSize], ElemSize);
+  }
+
+  return scrambledArr;
+}
+
+int* getIntArr(int* intSize) {
+  int arrLogSize = 0;
+  int arrPhySize = 1;
+  int* intArr = (int*)malloc(arrPhySize * sizeof(int));
+
+  scanf("%d ", intSize);
+
+  for (int i = 0; i < *intSize; i++) {
+    intArr = increaseArraySizeIfFull(intArr, &arrLogSize, &arrPhySize, sizeof(int));
+    scanf("%d", &intArr[arrLogSize]);
+    arrLogSize++;
+  }
+
+  intArr = (int*)realloc(intArr, arrLogSize * sizeof(int));
+  return intArr;
+}
+
+int* scrambleInt(int* intArr, int intSize, int* indArr) {
+  int* scrambledArr = (int*)scramble(intArr, sizeof(int), intSize, indArr);
+
+  return scrambledArr;
+}
+
+void printIntArr(int* intArr, int intSize) {
+  int i;
+
+  for (i = 0; i < intSize; i++) {
+    printf("%d ", intArr[i]);
+  }
+  printf("\n");
+}
+
+char** getStringArr(int* stringArrSize) {
+  int arrLogSize = 0;
+  int arrPhySize = 1;
+  char** stringArr = (char**)malloc(arrPhySize * sizeof(char*));
+
+  scanf("%d ", stringArrSize);
+
+  for (int i = 0; i < *stringArrSize; i++) {
+    stringArr = increaseArraySizeIfFull(stringArr, &arrLogSize, &arrPhySize, sizeof(char*));
+
+    stringArr[arrLogSize] = (char*)malloc(STRING_MAX_LENGTH * sizeof(char));
+    checkAllocation(stringArr[arrLogSize]);
+
+    gets(stringArr[arrLogSize]);
+
+    arrLogSize++;
+  }
+
+  stringArr = (char**)realloc(stringArr, arrLogSize * sizeof(char*));
+  return stringArr;
+}
+
+char** scrambleString(char** stringArr, int stringArrSize, int* indArr) {
+  return (char**)scramble(stringArr, sizeof(char*), stringArrSize, indArr);
+}
+
+void printStringArr(char** stringArr, int stringArrSize) {
+  for (int i = 0; i < stringArrSize; i++) {
+    printf("%s\n", stringArr[i]);
+  }
+}
+
+void freeMemory(int* intArr, int* scrambleIntArr, int intSize, char** stringArr, char** scrambleStringArr,
+                int stringSize) {
+  free(intArr);
+  free(scrambleIntArr);
+  for (int i = 0; i < stringSize; i++) {
+    free(stringArr[i]);
+  }
+  free(stringArr);
+  free(scrambleStringArr);
+}
--- common.h
+++ common.h
@@ -6,13 +6,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NOT_FOUND -1
-#define NUM_OF_BITS_IN_BYTE 8
+#define STRING_MAX_LENGTH 99
 
 typedef unsigned char BYTE;
 
 void checkAllocation(void* ptr);
 void checkFile(FILE* file);
 void* increaseArraySizeIfFull(void* arr, int* logSize, int* phySize, unsigned long sizeOfType);
+void* scramble(void* arr, int ElemSize, int n, int* indArr);
+int* getIntArr(int* intSize);
+int* scrambleInt(int* intArr, int intSize, int* indArr);
+void printIntArr(int* intArr, int intSize);
+char** getStringArr(int* stringArrSize);
+char** scrambleString(char** stringArr, int stringSize, int* indArr);
+void printStringArr(char** stringArr, int stringSize);
+void freeMemory(int* intArr, int* scrambleIntArr, int intSize, char** stringArr, char** scrambleStringArr,
+                int stringSize);
 
 #endif // _COMMON_H
