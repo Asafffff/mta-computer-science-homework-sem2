0745efdbc0a96d821313b13fbab3dc07295e4851
Asaf Kfir <asafkfir11@gmail.com>
2022-05-21 15:06:46 +0300

    Implement filter and xor filter

--- common.c
+++ common.c
@@ -31,28 +31,88 @@ void* increaseArraySizeIfFull(void* arr, int* logSize, int* phySize, unsigned lo
   return arr;
 }
 
-BYTE* compressStringInteger(char* integerAsString, int numOfBytes) {
-  int i;
-  BYTE* compressedString = (BYTE*)malloc(sizeof(BYTE) * numOfBytes);
-  checkAllocation(compressedString);
+bool isBitOnByIndex(BYTE byte, unsigned int index) {
+  BYTE mask = 0x01;
+  mask <<= index;
 
-  int stringLength = strlen(integerAsString);
-  BYTE currentByte = 0x00;
+  return (byte & mask) > 0;
+}
 
-  for (i = 0; i < stringLength; i++) {
-    char currentDigit = integerAsString[i] - '0';
+/**
+ * @brief filter, check an array of bits, pred1, and only append numbers that complies with that that only
+ * the current bit is turned on.
+ *
+ * @param Numbers array of numbers
+ * @param size size of Numbers array
+ * @param pred1 first bits filter
+ * @param new_size output parameter for the size of the new array
+ * @return int* returned array
+ */
+int* filter(int* Numbers, int size, unsigned char* pred, int* new_size) {
+  int i, j;
 
-    // If the index is even, it should take the 4 leftmost bits
-    if (i % 2 == 0) {
-      currentByte |= currentDigit << 4;
-    } else {
-      currentByte |= currentDigit;
+  int filteredNumbersLogSize = 0;
+  int* filteredNumbers = (int*)malloc(size * sizeof(int));
 
-      // Append to compressedId and reset state
-      compressedString[i / 2] = currentByte;
-      currentByte = 0x00;
+  for (i = 0; i < size / 8; i++) {
+    BYTE currentByte = pred[i];
+
+    // Loop backwards from 7 to 0
+    for (j = NUM_OF_BITS_IN_BYTE - 1; j >= 0; j--) {
+      // Check if bit is on, on given index
+      if (isBitOnByIndex(currentByte, NUM_OF_BITS_IN_BYTE - 1 - j)) {
+        // append the correct number to the output array
+        int currentNumber = Numbers[i * 8 + NUM_OF_BITS_IN_BYTE - 1 - j];
+        filteredNumbers[filteredNumbersLogSize] = currentNumber;
+        filteredNumbersLogSize++;
+      }
     }
   }
 
-  return compressedString;
+  filteredNumbers = (int*)realloc(filteredNumbers, filteredNumbersLogSize * sizeof(int));
+
+  *new_size = filteredNumbersLogSize;
+  return filteredNumbers;
+}
+
+/**
+ * @brief xorFilter, check two array of bits, pred1 and pred2, and only append numbers that complies with that that only
+ * one of them is turned on.
+ *
+ * @param Numbers array of numbers
+ * @param size size of Numbers array
+ * @param pred1 first bits filter
+ * @param pred2 second bits filter
+ * @param new_size output parameter for the size of the new array
+ * @return int* returned array
+ */
+int* xorFilter(int* Numbers, int size, unsigned char* pred1, unsigned char* pred2, int* new_size) {
+  int i, j;
+
+  int filteredNumbersLogSize = 0;
+  int* filteredNumbers = (int*)malloc(size * sizeof(int));
+
+  for (i = 0; i < size / 8; i++) {
+    BYTE currentPred1Byte = pred1[i];
+    BYTE currentPred2Byte = pred2[i];
+
+    // Loop backwards from 7 to 0
+    for (j = NUM_OF_BITS_IN_BYTE - 1; j >= 0; j--) {
+      // Check each filter bit
+      bool pred1BitIsOn = isBitOnByIndex(currentPred1Byte, NUM_OF_BITS_IN_BYTE - 1 - j);
+      bool pred2BitIsOn = isBitOnByIndex(currentPred2Byte, NUM_OF_BITS_IN_BYTE - 1 - j);
+
+      if ((pred1BitIsOn && !pred2BitIsOn) || (!pred1BitIsOn && pred2BitIsOn)) {
+        // append the correct number to the output array
+        int currentNumber = Numbers[i * 8 + NUM_OF_BITS_IN_BYTE - 1 - j];
+        filteredNumbers[filteredNumbersLogSize] = currentNumber;
+        filteredNumbersLogSize++;
+      }
+    }
+  }
+
+  filteredNumbers = (int*)realloc(filteredNumbers, filteredNumbersLogSize * sizeof(int));
+
+  *new_size = filteredNumbersLogSize;
+  return filteredNumbers;
 }
--- common.h
+++ common.h
@@ -7,13 +7,15 @@
 #include <string.h>
 
 #define NOT_FOUND -1
+#define NUM_OF_BITS_IN_BYTE 8
 
 typedef unsigned char BYTE;
 
 void checkAllocation(void* ptr);
 void checkFile(FILE* file);
 void* increaseArraySizeIfFull(void* arr, int* logSize, int* phySize, unsigned long sizeOfType);
-BYTE* compressStringInteger(char* integerAsString, int numOfBytes);
+bool isBitOnByIndex(BYTE byte, unsigned int index);
 int* filter(int* Numbers, int size, unsigned char* pred, int* new_size);
+int* xorFilter(int* Numbers, int size, unsigned char* pred1, unsigned char* pred2, int* new_size);
 
 #endif // _COMMON_H
