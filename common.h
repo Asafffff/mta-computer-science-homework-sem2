f6c047ff374fcd34e323d160298d16439189f6d4
Asaf Kfir <asafkfir11@gmail.com>
2022-05-21 14:14:35 +0300

    Update base conditions

--- common.h
+++ common.h
@@ -7,34 +7,13 @@
 #include <string.h>
 
 #define NOT_FOUND -1
-#define SHORT_ID_LENGTH 4
-#define SHORT_PHONE_LENGTH 5
-#define PHONE_NUMBER_DASH_INDEX 3
 
 typedef unsigned char BYTE;
-
-typedef struct _client {
-  char id[9];     // “12345678”
-  char phone[12]; // “054-1234567”
-} Client;
-
-typedef struct _short_client {
-  unsigned char short_id[4];
-  unsigned char short_phone[5];
-} Short_client;
 
 void checkAllocation(void* ptr);
 void checkFile(FILE* file);
 void* increaseArraySizeIfFull(void* arr, int* logSize, int* phySize, unsigned long sizeOfType);
-Client* getClientByInput();
-void normalizePhoneNumber(char* phoneNumber);
-void denormalizePhoneNumber(char* phoneNumber);
 BYTE* compressStringInteger(char* integerAsString, int numOfBytes);
-BYTE* compressId(char* clientId);
-BYTE* compressPhone(char* clientPhone);
-Short_client* compressClients(Client clients[], int numOfClients);
-Short_client* createShortClientArr(int numOfClients);
-char* searchClientByID(Short_client* shortClients, int numOfClients, char* id);
-int getClientIndexByCompressedId(Short_client* shortClients, int numOfClients, BYTE* compressedId);
+int* filter(int* Numbers, int size, unsigned char* pred, int* new_size);
 
 #endif // _COMMON_H
--- main.c
+++ main.c
@@ -2,24 +2,64 @@
 #include "common.h"
 
 void main() {
-  int n;
-  Short_client* arr;
-  char id[9];
-  char* phone;
+  int i, pr, new_size, size;
+  int* numbers;
+  unsigned char *pred1, *pred2;
+  int* some_numbers1;
+  int* some_numbers2;
 
-  printf("Please enter the number of clients: ");
-  scanf("%d", &n);
+  printf("Please enter the number of numbers:\n");
+  scanf("%d", &size);
 
-  arr = createShortClientArr(n);
-  getc(stdin);
-  gets(id);
-  phone = searchClientByID(arr, n, id);
-  if (phone == NULL) {
-    printf("Can not find client with i.d. %s\n", id);
-  } else {
-    printf("The phone number of client i.d. %s is %s\n", id, phone);
-    free(phone);
+  numbers = (int*)malloc(size * sizeof(int));
+
+  if (numbers == NULL)
+    exit(1);
+
+  pred1 = (char*)malloc(size / 8 * sizeof(char));
+
+  if (pred1 == NULL)
+    exit(1);
+
+  pred2 = (char*)malloc(size / 8 * sizeof(char));
+
+  if (pred2 == NULL)
+    exit(1);
+
+  printf("Please enter the numbers:\n");
+
+  for (i = 0; i < size; i++)
+    scanf("%d", &numbers[i]);
+
+  printf("Please enter the first filter bits in hexa:\n");
+
+  for (i = 0; i < size / 8; i++) {
+    scanf("%x", &pr);
+    pred1[i] = (char)pr;
   }
 
-  free(arr);
+  some_numbers1 = filter(numbers, size, pred1, &new_size);
+  printf("The numbers selected are:\n");
+
+  for (i = 0; i < new_size; i++)
+    printf("%d ", some_numbers1[i]);
+
+  printf("Please enter the second filter bits in hexa:\n");
+
+  for (i = 0; i < size / 8; i++) {
+    scanf("%x", &pr);
+    pred2[i] = (char)pr;
+  }
+
+  some_numbers2 = xorFilter(numbers, size, pred1, pred2, &new_size);
+  printf("The numbers selected are:\n");
+
+  for (i = 0; i < new_size; i++)
+    printf("%d ", some_numbers2[i]);
+
+  free(numbers);
+  free(some_numbers1);
+  free(pred1);
+  free(some_numbers2);
+  free(pred2);
 }
